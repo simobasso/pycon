aeca5982d6e52bb2fc75994ce7fcb57ae74375e6
Marco Acierno <marcoacierno96@gmail.com>
2017-05-02 23:08:15 +0200

    Don't serve wagtail pages

--- backend/pycon/urls.py
+++ backend/pycon/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
-from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 
@@ -14,6 +13,4 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^django-admin/', admin.site.urls),
-
-    url(r'', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
